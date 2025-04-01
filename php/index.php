c5ce953728588fde2651c58b70494b1067cfa56f
Makwana Raj <151895608+raj-mistry-01@users.noreply.github.com>
2025-04-01 18:16:12 +0530

    2nd

--- php/index.php
+++ php/index.php
@@ -1,14 +1,35 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "vuln_db");
-if ($conn->connect_error) die("Connection failed");
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 
 if (isset($_POST['login'])) {
     $user = $_POST['username'];
     $pass = $_POST['password'];
     $query = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'";
     $result = $conn->query($query);
+    if (!$result) {
+        die("Query failed: " . $conn->error);
+    }
     if ($result->num_rows > 0) {
-        echo "Login successful!";
+        // Login successful, fetch all usernames and passwords
+        $data_query = "SELECT username, password FROM users";
+        $data_result = $conn->query($data_query);
+        if (!$data_result) {
+            die("Data query failed: " . $conn->error);
+        }
+
+        // Store the data in an array to pass to JavaScript
+        $users_data = [];
+        while ($row = $data_result->fetch_assoc()) {
+            $users_data[] = $row;
+        }
+
+        // Output "Login successful!" and include JavaScript to log the data
+        echo "Login successful!<br>";
+        echo "<script>";
+        echo "console.log('Hacker accessed user data:');";
+        echo "console.log(" . json_encode($users_data) . ");";
+        echo "</script>";
     } else {
         echo "Login failed.";
     }
